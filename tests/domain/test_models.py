fc0d56adfd065a5d52fe26cf3d3524ea14c7ec63
Dashgin <dsdsds2103@gmail.com>
2024-08-17 11:32:56 +0400

    feat: add `operands_factory` function and related test

--- tests/domain/test_models.py
+++ tests/domain/test_models.py
@@ -1,7 +1,13 @@
-from src.pytemplate.domain.models import Operands
+from src.pytemplate.domain.models import Operands, operands_factory
 
 
 def test_Operands():
     operands = Operands(first_operand=1, second_operand=2)
+    assert operands.first_operand == 1
+    assert operands.second_operand == 2
+
+
+def test_operands_factory():
+    operands = operands_factory(1, 2)
     assert operands.first_operand == 1
     assert operands.second_operand == 2
